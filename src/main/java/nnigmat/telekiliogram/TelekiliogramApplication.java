92f0820258784de6f25d56b0addbab10d15351b3
Nikita <nnigmat@gmail.com>
2019-06-06 12:01:46 +0300

    Initial project setup

---
+++ src/main/java/nnigmat/telekiliogram/TelekiliogramApplication.java
@@ -0,0 +1,13 @@
+package nnigmat.telekiliogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelekiliogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelekiliogramApplication.class, args);
+	}
+
+}
