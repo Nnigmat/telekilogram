f2cda41816bebb14aa7c1769c496c9f0f88dc2fb
Nikita <nnigmat@gmail.com>
2019-06-06 18:10:03 +0300

    Add Role.java

---
+++ src/main/java/nnigmat/telekilogram/domain/Role.java
@@ -0,0 +1,10 @@
+package nnigmat.telekilogram.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, MODERATOR, ADMIN;
+
+    @Override
+    public String getAuthority() { return name(); }
+}
