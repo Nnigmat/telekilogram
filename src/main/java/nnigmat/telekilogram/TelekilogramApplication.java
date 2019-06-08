7119439864db835209909261f837d33f71e47e6a
Nikita <nnigmat@gmail.com>
2019-06-08 15:00:44 +0300

    Init

---
+++ src/main/java/nnigmat/telekilogram/TelekilogramApplication.java
@@ -0,0 +1,13 @@
+package nnigmat.telekilogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelekilogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelekilogramApplication.class, args);
+    }
+
+}
