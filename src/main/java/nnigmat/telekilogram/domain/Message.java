1ddf31996a944ec474aa8f16e96c8dcaf5ad7ed9
Nikita <nnigmat@gmail.com>
2019-06-08 17:02:12 +0300

    Add Message.java ORM class

---
+++ src/main/java/nnigmat/telekilogram/domain/Message.java
@@ -0,0 +1,53 @@
+package nnigmat.telekilogram.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
