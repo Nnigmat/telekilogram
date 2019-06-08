92d6c1a4f785bb3cc2ec8b7901a0f46fcb72fee4
Nikita <nnigmat@gmail.com>
2019-06-08 17:40:44 +0300

    Add RoomController. Fix authentication problem

--- src/main/java/nnigmat/telekilogram/controller/RoomController.java
+++ src/main/java/nnigmat/telekilogram/controller/RoomController.java
@@ -1,5 +1,11 @@
 package nnigmat.telekilogram.controller;
 
+import nnigmat.telekilogram.domain.Message;
+import nnigmat.telekilogram.domain.Room;
+import nnigmat.telekilogram.domain.User;
+import nnigmat.telekilogram.repos.MessageRepo;
+import nnigmat.telekilogram.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +18,20 @@ import java.security.Principal;
 @RequestMapping("/room")
 public class RoomController {
 
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping
     public String listMessages(Principal principal, Model model) {
+        User userFromDb = userRepo.findByUsername(principal.getName());
+        Room roomFromDb = userFromDb.getCurrent_room();
+
+        Iterable<Message> messages = messageRepo.findByRoom(roomFromDb);
+        model.addAttribute("messages", messages);
+        model.addAttribute("user", userFromDb);
 
         return "room";
     }
--- src/main/java/nnigmat/telekilogram/repos/MessageRepo.java
+++ src/main/java/nnigmat/telekilogram/repos/MessageRepo.java
@@ -1,7 +1,11 @@
 package nnigmat.telekilogram.repos;
 
 import nnigmat.telekilogram.domain.Message;
+import nnigmat.telekilogram.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface MessageRepo extends JpaRepository<Message, Long> {
+    Collection<Message> findByRoom(Room room);
 }
