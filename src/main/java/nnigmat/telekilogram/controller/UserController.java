21840e4e27cdb1d831240ef66602b86b4bbbf12f
Nikita <nnigmat@gmail.com>
2019-06-09 13:01:48 +0300

    Add /user where admins and moderators can give privileges to the users

--- src/main/java/nnigmat/telekilogram/controller/UserController.java
+++ src/main/java/nnigmat/telekilogram/controller/UserController.java
@@ -1,5 +1,6 @@
 package nnigmat.telekilogram.controller;
 
+import nnigmat.telekilogram.domain.Role;
 import nnigmat.telekilogram.domain.User;
 import nnigmat.telekilogram.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/user")
@@ -22,7 +28,37 @@ public class UserController {
         Iterable<User> users = userRepo.findAll();
 
         model.addAttribute("users", users);
+        model.addAttribute("Role", Role.class);
 
         return "user_list";
+    }
+
+    @PostMapping("/give_ban/{user}")
+    public String ban(@PathVariable User user){
+        Set<Role> ban = new HashSet<Role>();
+        if (user.getAuthorities().contains(Role.BAN)) {
+            ban.add(Role.USER);
+        } else {
+            ban.add(Role.BAN);
+        }
+        user.setRoles(ban);
+        userRepo.save(user);
+
+        return "redirect:/user";
+    }
+
+    @PostMapping("/give_moderator/{user}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String moderator(@PathVariable User user){
+        Set<Role> ban = new HashSet<Role>();
+        if (user.getAuthorities().contains(Role.MODERATOR)) {
+            ban.add(Role.USER);
+        } else {
+            ban.add(Role.MODERATOR);
+        }
+        user.setRoles(ban);
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 }
